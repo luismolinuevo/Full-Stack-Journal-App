aa3377d902f400011da81668b44aba93e80d835f
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-02-04 02:03:34 -0500

    fixed login route, hid info

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -7,26 +7,43 @@ router.post("/signup", async (req,res) => {
     console.log("POST body: ", req.body);
     const hashedPassword = await bcrypt.hash(password, 10);
     const User = new user({ name, email, password: hashedPassword });
-    await User.save()
-    .then((user) => {
-        user.password = undefined;
-        // req.login(user, () => res.status(201).json(user));
-      })
-      .catch((err) => {
-        res.status(400).json({ msg: "Failed Signup", err });
-      });
-})
+    try {
+      const newuser = await User.save();
+      newuser.password = undefined;
+      res.status(201).json(newuser)
+    } catch(err) {
+      res.status(400).json({ msg: "Failed Signup", err });
+    }
 
+})
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const User = await user.findOne({ email });
-  if (!user) return res.status(400).send("Invalid email or password");
+  console.log(User);
+  if (!User) {
+    console.log("no user found")
+    return res.status(401).send("Invalid email or password");
+  }
   const valid = await bcrypt.compare(password, User.password);
-  if (!valid) return res.status(400).send("Invalid email or password");
+  if (!valid) {
+    console.log("error")
+    return res.status(401).send("Invalid email or password");
+  }
   req.session.user = User;  //uncommenting this fixed the error I had with adding entries.(This opens a session if the data of the user that logged in)
   // req.session.userId = User._id
-  res.send("Logged In");
+  res.status(200).json(User);   //send the data and status. Have to send status cause in front end we are checking if we are status 200
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.user) {   //putting this fixed it
+    res.json(req.session.user);
+    console.log(req.session.user);
+  } else {
+    res.sendStatus(401);
+  }
+
+  // if(req.session.user && req.session.user.email)
 });
 
 

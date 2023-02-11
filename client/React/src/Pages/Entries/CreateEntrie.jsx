7a93bdb00b704b447ac1363c42c8760a6a57a45f
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-02-10 21:16:00 -0500

    mood picker setup

--- client/React/src/Pages/Entries/CreateEntrie.jsx
+++ client/React/src/Pages/Entries/CreateEntrie.jsx
@@ -10,19 +10,23 @@ export default function CreateEntrie() {
     const [explainMood, setExplainMood] = useState("");
     const [whatHappened, setWhatHappened] = useState("");
 
-    const[isHappy, setIsHappy] = useState(false);
-    const[isInLove, setInLove] = useState(false);
-    const[isMeh, setIsMeh] = useState(false);
-    const[isSad, setIsSad] = useState(false);
-    const[isMad, setIsMad] = useState(false);
+    // const[isHappy, setIsHappy] = useState(false);
+    // const[isInLove, setInLove] = useState(false);
+    // const[isMeh, setIsMeh] = useState(false);
+    // const[isSad, setIsSad] = useState(false);
+    // const[isMad, setIsMad] = useState(false);
 
-    const happyOnClick = () => {
-        // if(mood.includes("happy"))
-        setIsHappy((prev) => !prev);
-        if(isHappy) {
-            setMood([...mood, "happy"]);
+    const moodHandler = (emotion) => { //emotion could be "happy", "sad", "rad", etc
+        const moodFound = mood.includes(emotion);
+        if(!moodFound) {
+            setMood([...mood, emotion]);
+            console.log("added");
+
+        } else {
+            setMood(mood.filter(word => word !== emotion));
+            console.log("removed")
         }
-        // setMood("happy")
+        console.log(mood);
     }
 
     const sumbitForm = () => {
@@ -42,31 +46,35 @@ export default function CreateEntrie() {
             <div className="moodPicker">
                 <h1>Mood:</h1>
                 <div className='moodEmjois'>
-                    <div className='moods'>
-                        <button><BsEmojiSmile/></button>
+                    <div className = {mood.includes("rad") ? "moods selected" : "moods"}>
+                        <button onClick={() => moodHandler("rad")}
+                        >
+                            <BsEmojiSmile/>
+                        </button>
                         <p>rad</p>
                     </div>
-                    <div className='moods'>
-                        <button><BsEmojiHeartEyes/></button> 
+                    <div className = {mood.includes("love") ? "moods selected" : "moods"}>
+                        <button onClick={() => moodHandler("love")}><BsEmojiHeartEyes/></button> 
                         <p>love</p>
                     </div>
-                    <div className='moods'>
-                        <button><BsEmojiNeutral/></button>
+                    <div className = {mood.includes("meh") ? "moods selected" : "moods"}>
+                        <button onClick={() => moodHandler("meh")}><BsEmojiNeutral/></button>
                         <p>meh</p>
                     </div>
-                    <div className='moods'>
-                        <button><BsEmojiFrown/></button> 
+                    <div className = {mood.includes("sad") ? "moods selected" : "moods"}>
+                        <button onClick={() => moodHandler("sad")}><BsEmojiFrown/></button> 
                         <p>sad</p>
                     </div>
-                    <div className='moods'>
-                        <button><BsEmojiAngry/></button> 
+                    <div className = {mood.includes("mad") ? "moods selected" : "moods"}>
+                        <button onClick={() => moodHandler("mad")}><BsEmojiAngry/></button> 
                         <p>mad</p>
                     </div>
                 </div>
                 
             </div>
         </div>
-        <div>
+        <div className='titleContainer'>
+            <h1>Title:</h1>
             <input type="text" value={title} placeholder="Enter title"  onChange={event => {title(event.target.value)}}/>
         </div>
         <div>

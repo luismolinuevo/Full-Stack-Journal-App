6467ac375e1fb1c0caaa94607715bed9b5260402
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-02-14 00:56:16 -0500

    created and styled homepage for desktop computer

--- client/React/src/Pages/Home/Home.jsx
+++ client/React/src/Pages/Home/Home.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react'
+import { useAuth } from "../../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import "./Home.css"
 
-export class Home extends Component {
-  render() {
+export default function Home() {
+  const auth = useAuth();  //from authcontext
+  const navigate = useNavigate();
+
+  const login = async (e) => {
+    let email = "test1@gmail.com"
+    let password = "Luis111"
+    e.preventDefault();
+    // let { email, password } = data;    //makes email and password variable the email and password that is in the data state           
+  
+    try {
+      await auth.authenticate(email, password);  //authenticate from authcontext 
+      //  navigate(from, { replace: true });   //go to where user was before login
+          navigate("/")
+          
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+
     return (
       <div id="home">
         <div className='container'>
@@ -10,16 +32,14 @@ export class Home extends Component {
             <p>
               This app allows you to create journal entries and track your mood at the same time.
               Allowing for better day to day reflection and expressing your emotions with a couple clicks.
-              <div className="homeButtons">
-                
-              </div>
             </p>
-
+            <div className="homeButtons">
+              <Link to={"/signup"} className="getStarted">Get Started!</Link>
+              <button onClick={login} className="demoButton">Demo App</button>
+            </div>
           </div>
         </div>
       </div>
     )
   }
-}
 
-export default Home;
